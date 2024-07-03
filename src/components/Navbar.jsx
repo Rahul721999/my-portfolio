b1903abe9f7d7b65957e2c2142e0d0e3e9b93fa2
Rahul721999 <rrmondal12@gmail.com>
2024-07-03 12:59:53 +0530

    added: glow effect to the buttons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,11 +8,11 @@ const Navbar = ({ toggleTheme, theme }) => {
         <header className="fixed top-0 left-0 w-full z-100 padding-x-10p p-[20px] flex items-center justify-between bg-transparent">
             <a className="text-2xl font-bold ">Rahul.</a>
             <nav className="flex">
-                <a href="#" className="text-lg font-medium  mr-9 transition-colors duration-300 hover:text-cyan">Home</a>
-                <a href="#" className="text-lg font-medium  mr-9 transition-colors duration-300 hover:text-cyan">About</a>
-                <a href="#" className="text-lg font-medium  mr-9 transition-colors duration-300 hover:text-cyan">Service</a>
-                <a href="#" className="text-lg font-medium  mr-9 transition-colors duration-300 hover:text-cyan">Portfolio</a>
-                <a href="#" className="text-lg font-medium  mr-9 transition-colors duration-300 hover:text-cyan">Contact</a>
+                <a href="#" className="text-lg font-medium  mr-9 transition-colors duration-300 hover:text-cyan nav-bar-component">Home</a>
+                <a href="#" className="text-lg font-medium  mr-9 transition-colors duration-300 hover:text-cyan nav-bar-component">About</a>
+                <a href="#" className="text-lg font-medium  mr-9 transition-colors duration-300 hover:text-cyan nav-bar-component">Service</a>
+                <a href="#" className="text-lg font-medium  mr-9 transition-colors duration-300 hover:text-cyan nav-bar-component">Portfolio</a>
+                <a href="#" className="text-lg font-medium  mr-9 transition-colors duration-300 hover:text-cyan nav-bar-component">Contact</a>
                 <button onClick={toggleTheme} className="flex rounded mr-9">
                     <img src={themeIcon} alt="Theme Icon" className="w-0.375rm h-0.375rem justify-center" />
                 </button>

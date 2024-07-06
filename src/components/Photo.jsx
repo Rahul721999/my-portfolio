aef6d5df33ca4023e2742881cfc02ed4fb4ac209
Rahul721999 <rrmondal12@gmail.com>
2024-07-06 20:47:52 +0530

    fixing: trying to fix photo render error on deployment

--- src/components/Photo.jsx
+++ src/components/Photo.jsx
@@ -3,23 +3,23 @@ const Photo = () => {
     return (
         <div className="hidden justify-center items-center rounded-full laptop:flex laptop:w-2/5">
             <img
-                src='src\assets\stand_photo.jpg'
-                alt='profile-picture'
+                src='\large_photo.png'
+                alt='desktop-profile'
                 className='rounded-full'
             />
         </div>
     )
 }
 
-const MobilePhoto = () =>{
-    return(
+const MobilePhoto = () => {
+    return (
         <div className="flex justify-center items-center rounded-full laptop:hidden">
             <img
-                src='src\assets\photo_small.jpg'
-                alt='profile-picture'
+                src='\small_photo.png'
+                alt='mobile-profile'
                 className='rounded-full'
             />
         </div>
     )
 }
-export {Photo, MobilePhoto};
+export { Photo, MobilePhoto };

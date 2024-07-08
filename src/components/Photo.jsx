5c0adca7d8e09a624fe01ead2e079b3a6b72617e
Rahul721999 <rrmondal12@gmail.com>
2024-07-08 15:36:12 +0530

    minor changes

--- src/components/Photo.jsx
+++ src/components/Photo.jsx
@@ -3,7 +3,7 @@ const Photo = () => {
     return (
         <div className="hidden justify-center items-center rounded-full laptop:flex profile-photo">
             <img
-                src='./src/assets/large_photo.png'
+                src='./large_photo.png'
                 alt='desktop-profile'
                 className='rounded-full'
             />
@@ -15,7 +15,7 @@ const MobilePhoto = () => {
     return (
         <div className="flex justify-center items-center rounded-full laptop:hidden profile-photo">
             <img
-                src='./src/assets/small_photo.png'
+                src='./small_photo.png'
                 alt='mobile-profile'
                 className='rounded-full'
             />

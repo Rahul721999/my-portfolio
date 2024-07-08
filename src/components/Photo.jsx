38ca749ab59734842d68d6464fb9daadab5f2bab
Rahul721999 <rrmondal12@gmail.com>
2024-07-08 14:59:27 +0530

    fixing: photo not rendering after deployment(wip)

--- src/components/Photo.jsx
+++ src/components/Photo.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 const Photo = () => {
     return (
-        <div className="hidden justify-center items-center rounded-full laptop:flex">
+        <div className="hidden justify-center items-center rounded-full laptop:flex profile-photo">
             <img
                 src='./src/assets/large_photo.png'
                 alt='desktop-profile'
@@ -13,7 +13,7 @@ const Photo = () => {
 
 const MobilePhoto = () => {
     return (
-        <div className="flex justify-center items-center rounded-full laptop:hidden">
+        <div className="flex justify-center items-center rounded-full laptop:hidden profile-photo">
             <img
                 src='./src/assets/small_photo.png'
                 alt='mobile-profile'

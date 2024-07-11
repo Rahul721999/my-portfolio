0f60a8d5b71fd9820f0de668271502caec061549
Rahul721999 <rrmondal12@gmail.com>
2024-07-11 09:33:57 +0530

    added: more skills

--- src/pages/Skillspage.jsx
+++ src/pages/Skillspage.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Scroller } from '../components';
 
 export default function Skills() {
-    const skills = ['HTML', 'CSS', 'TailWind', 'BootStrap', 'JS', 'Node', 'Express', 'MongoDB', 'PostgreSQL', 'RUST', 'Actix', 'WARP', 'AXUM'];
+    const skills = ['HTML', 'CSS', 'TailWind', 'BootStrap', 'JS', 'Node', 'Express', 'MongoDB', 'PostgreSQL', 'RUST', 'Actix', 'WARP', 'AXUM','Python', 'TensorFlow', 'WebSocket', 'JWT', 'Hashing', 'Encryption', 'Asym-Crypto','Git', 'GitHub', 'GitLab', 'BitBucket', 'Docker', 'Kubernetes', 'Okteto', 'Ingress', 'Linux', 'Solana', 'gRPC', 'MicroService'];
 
     return (
         <section id='skills' className='section-container secondary-color'>

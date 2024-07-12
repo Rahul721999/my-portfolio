85c7190c2da1789587797d0c0edcc767169c11b7
Rahul721999 <rrmondal12@gmail.com>
2024-07-12 22:06:25 +0530

    added: contact-me page

--- src/pages/Contact/Contactpage.jsx
+++ src/pages/Contact/Contactpage.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React from 'react';
+import './Contactpage.css';
+
 
 export default function Contact() {
     return (
         <section id='contact' className='section-container'>
-            <h1 className='text-4xl font-bold flex justify-center laptop:text-6xl items-center'>Contact&nbsp;<span className='text-cyan'>Me</span></h1>
+            <h1 className='text-4xl font-bold flex justify-center laptop:text-6xl items-center'>Contact&nbsp;<span className='text-cyan'>Me!</span></h1>
+
+            <form className='contact-form'>
+                <div className='input-box'>
+                    <div className='input-field'>
+                        <input type='text' placeholder='Full Name' required></input>
+                        <span className='focus'></span>
+                    </div>
+                    <div className='input-field'>
+                        <input type='text' placeholder='Email address' required></input>
+                        <span className='focus'></span>
+                    </div>
+                </div>
+                <div className='input-box'>
+                    <div className='input-field'>
+                        <input type='number' placeholder='Mobile number' required></input>
+                        <span className='focus'></span>
+                    </div>
+                    <div className='input-field'>
+                        <input type='text' placeholder='Email Subject' required></input>
+                        <span className='focus'></span>
+                    </div>
+                </div>
+                <div className='textarea-field'>
+                    <textarea name="" id='' cols={30} rows={10} placeholder='Type Your Message Here' required></textarea>
+                    <span className='focus'></span>
+                </div>
+                <div className="w-full h-12 flex justify-center">
+                    <a href="#" type="submit" className="inline-flex justify-center items-center w-36 h-full text-lg rounded-lg border-solid font-bold tracking-widest relative overflow-hidden z-1 border-2 border-cyan bg-cyan text-dark-theme cursor-pointer hire-me-btn">Submit</a>
+                </div>
+            </form>
         </section>
-      )
+    )
 }

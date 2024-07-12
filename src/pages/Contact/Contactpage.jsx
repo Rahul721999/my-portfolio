5d4b89ab61b8cd428ead0539506e2612aa7f719b
Rahul721999 <rrmondal12@gmail.com>
2024-07-12 23:53:45 +0530

    added: animation to contact-me form

--- src/pages/Contact/Contactpage.jsx
+++ src/pages/Contact/Contactpage.jsx
@@ -10,27 +10,27 @@ export default function Contact() {
             <form className='contact-form'>
                 <div className='input-box'>
                     <div className='input-field'>
-                        <input type='text' placeholder='Full Name' required></input>
-                        <span className='focus'></span>
+                        <input className='input-tag' type='text' placeholder='Full Name' required/>
+                        <span className='selected-field'></span>
                     </div>
                     <div className='input-field'>
-                        <input type='text' placeholder='Email address' required></input>
-                        <span className='focus'></span>
+                        <input className='input-tag' type='text' placeholder='Email address' required/>
+                        <span className='selected-field'></span>
                     </div>
                 </div>
                 <div className='input-box'>
                     <div className='input-field'>
-                        <input type='number' placeholder='Mobile number' required></input>
-                        <span className='focus'></span>
+                        <input className='input-tag' type='number' placeholder='Mobile number' required/>
+                        <span className='selected-field'></span>
                     </div>
                     <div className='input-field'>
-                        <input type='text' placeholder='Email Subject' required></input>
-                        <span className='focus'></span>
+                        <input className='input-tag' type='text' placeholder='Email Subject' required/>
+                        <span className='selected-field'></span>
                     </div>
                 </div>
                 <div className='textarea-field'>
                     <textarea name="" id='' cols={30} rows={10} placeholder='Type Your Message Here' required></textarea>
-                    <span className='focus'></span>
+                    <span className='selected-field'></span>
                 </div>
                 <div className="w-full h-12 flex justify-center">
                     <a href="#" type="submit" className="inline-flex justify-center items-center w-36 h-full text-lg rounded-lg border-solid font-bold tracking-widest relative overflow-hidden z-1 border-2 border-cyan bg-cyan text-dark-theme cursor-pointer hire-me-btn">Submit</a>

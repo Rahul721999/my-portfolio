12dad939ea26144ea6da56da87ba837b1861a3f8
Rahul721999 <rrmondal12@gmail.com>
2024-07-14 23:27:34 +0530

    Added: functionality to contact-me form

--- src/components/SkillScroller/SkillScroller.jsx
+++ src/components/SkillScroller/SkillScroller.jsx
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 import './SkillScroller.css';
 
 export default function Scroller() {
-    const skills = ['HTML', 'CSS', 'TailWind', 'BootStrap', 'JS', 'NodeJs', 'ExpressJs', 'VueJs', 'VanillaJs','ReactJs', 'NextJs', 'MongoDB', 'PostgreSQL', 'RUST', 'Actix.rs', 'WARP.rs', 'AXUM.rs','Python', 'TensorFlow.py', 'WebSocket', 'JWT', 'Hashing', 'Encryption', 'Asym-Crypto','Git', 'GitHub', 'GitLab', 'BitBucket', 'Docker', 'Kubernetes', 'Okteto', 'Ingress', 'Linux', 'Solana', 'gRPC', 'MicroService', 'AI', 'ML', 'DL', 'CNN', 'LLM'];
+    const skills = ['HTML', 'CSS', 'TailWind', 'BootStrap', 'JS', 'NodeJs', 'ExpressJs', 'VueJs', 'VanillaJs', 'ReactJs', 'NextJs', 'MongoDB', 'PostgreSQL', 'RUST', 'Actix.rs', 'WARP.rs', 'AXUM.rs', 'Python', 'TensorFlow.py', 'WebSocket', 'JWT', 'Hashing', 'Encryption', 'Asym-Crypto', 'Git', 'GitHub', 'GitLab', 'BitBucket', 'Docker', 'Kubernetes', 'Okteto', 'Ingress', 'Linux', 'Solana', 'gRPC', 'MicroService', 'AI', 'ML', 'DL', 'CNN', 'LLM'];
 
     const totalItems = skills.length;
 
@@ -31,15 +31,30 @@ export default function Scroller() {
 }
 
 
-const IconScroller = () =>{
+const IconScroller = () => {
     const skills = [
 
-        
+
     ]
 
-    return(
-        <duv>
-
-        </duv>
+    return (
+        <div>
+            <div class="flex flex-wrap items-center justify-center max-w-5xl mx-auto overflow-x-auto scale-75 gap-y-8 gap-x-14">
+                <a href="/platforms/html-contact-form"><img title="HTML" alt="HTML" loading="lazy" width="50" height="50" decoding="async" data-nimg="1" style="color: transparent; --darkreader-inline-color: transparent;" src="/_next/static/media/html.7c2ef4a7.svg" data-darkreader-inline-color="" /> </a>
+                <a href="/platforms/javascript-contact-form"><img title="Javascript" alt="Javascript" loading="lazy" width="40" height="40" decoding="async" data-nimg="1" style="color: transparent; --darkreader-inline-color: transparent;" src="/_next/static/media/javascript.2369702c.svg" data-darkreader-inline-color="" /> </a>
+                <a href="/platforms/bootstrap-contact-form"><img title="Bootstrap" alt="Bootstrap" loading="lazy" width="60" height="60" decoding="async" data-nimg="1" style="color: transparent; --darkreader-inline-color: transparent;" src="/_next/static/media/bootstrap.f6377d14.svg" data-darkreader-inline-color="" /> </a>
+                <a href="/platforms/tailwindcss-contact-form"><img title="Tailwind CSS" alt="Tailwind CSS" loading="lazy" width="54" height="33" decoding="async" data-nimg="1" style="color: transparent; --darkreader-inline-color: transparent;" src="/_next/static/media/tailwindcss.cc59a337.svg" data-darkreader-inline-color="" /></a>
+                <a href="/platforms/react-contact-form"><img title="React" alt="React" loading="lazy" width="50" height="50" decoding="async" data-nimg="1" style="color: transparent; --darkreader-inline-color: transparent;" src="/_next/static/media/react.584ee5c3.svg" data-darkreader-inline-color="" /></a>
+                <a href="/platforms/nextjs-contact-form"><img title="Next.js" alt="Next.js" loading="lazy" width="105" height="65" decoding="async" data-nimg="1" style="color: transparent; --darkreader-inline-color: transparent;" src="/_next/static/media/nextjs.8e03c09e.svg" data-darkreader-inline-color="" /></a>
+                <a href="/platforms/astro-contact-form"><img title="Astro" alt="Astro" loading="lazy" width="85" height="107" decoding="async" data-nimg="1" class="w-8" style="color: transparent; --darkreader-inline-color: transparent;" src="/_next/static/media/astro.bc0209d3.svg" data-darkreader-inline-color="" /> </a>
+                <a href="/platforms/gatsby-contact-form"><img title="Gatsby" alt="Gatsby" loading="lazy" width="151" height="40" decoding="async" data-nimg="1" style="color: transparent; --darkreader-inline-color: transparent;" src="/_next/static/media/gatsby.3cd98233.svg" data-darkreader-inline-color="" /> </a>
+                <a href="/platforms/vue-contact-form"><img title="Vue.js" alt="Vue.js" loading="lazy" width="50" height="50" decoding="async" data-nimg="1" style="color: transparent; --darkreader-inline-color: transparent;" src="/_next/static/media/vue.41dac46b.svg" data-darkreader-inline-color="" /></a>
+                <a href="/platforms/nuxt-contact-form"><img title="Nuxt.js" alt="Nuxt.js" loading="lazy" width="159" height="28" decoding="async" data-nimg="1" style="color: transparent; --darkreader-inline-color: transparent;" src="/_next/static/media/nuxtjs.ff117e92.svg" data-darkreader-inline-color="" /></a>
+                <a href="/platforms/framer-contact-form"><img title="Framer" alt="Framer" loading="lazy" width="40" height="50" decoding="async" data-nimg="1" style="color: transparent; --darkreader-inline-color: transparent;" src="/_next/static/media/framer.9d70d7fd.svg" data-darkreader-inline-color="" /></a>
+                <a href="/platforms/alpinejs-contact-form"><img title="Alpine.js" alt="Alpine.js" loading="lazy" width="70" height="70" decoding="async" data-nimg="1" style="color: transparent; --darkreader-inline-color: transparent;" src="/_next/static/media/alpine.c7f73696.svg" data-darkreader-inline-color="" /></a>
+                <a href="/platforms/hugo-contact-form"><img title="Hugo" alt="Hugo" loading="lazy" width="154" height="41" decoding="async" data-nimg="1" style="color: transparent; --darkreader-inline-color: transparent;" src="/_next/static/media/hugo.734c91ed.svg" data-darkreader-inline-color="" /></a>
+                <a href="/platforms/svelte-contact-form"><img title="Svelte" alt="Svelte" loading="lazy" width="50" height="60" decoding="async" data-nimg="1" style="color: transparent; --darkreader-inline-color: transparent;" src="/_next/static/media/svelte.8bbfd60a.svg" data-darkreader-inline-color="" /></a>
+                <a href="/platforms/webflow-contact-form"><img title="Webflow" alt="Webflow" loading="lazy" width="50" height="50" decoding="async" data-nimg="1" style="color: transparent; --darkreader-inline-color: transparent;" src="/_next/static/media/webflow.4cae496c.svg" data-darkreader-inline-color="" /></a></div>
+        </div>
     )
 }
--- src/pages/Contact/Contactpage.jsx
+++ src/pages/Contact/Contactpage.jsx
@@ -3,37 +3,62 @@ import './Contactpage.css';
 
 
 export default function Contact() {
+    const [result, setResult] = React.useState("");
+    const secret_key = import.meta.env.VITE_SECRET_KEY;
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        setResult("Sending....");
+        
+        const formData = new FormData(event.target);
+        formData.append("access_key",secret_key);
+
+        const response = await fetch("https://api.web3forms.com/submit", {
+            method: "POST",
+            body: formData
+        });
+        const data = await response.json();
+
+        if (data.success) {
+            setResult("Form Submitted Successfully");
+            event.target.reset();
+        } else {
+            console.log("Error", data);
+            setResult(data.message);
+        }
+        console.log(result);
+    };
+
     return (
         <section id='contact' className='section-container'>
             <h1 className='text-4xl font-bold flex justify-center laptop:text-6xl items-center'>Contact&nbsp;<span className='text-cyan'>Me!</span></h1>
 
-            <form className='contact-form'>
+            <form className='contact-form' onSubmit={onSubmit}>
                 <div className='input-box'>
                     <div className='input-field'>
-                        <input type='text' placeholder='Full Name' required/>
+                        <input name="name" type='text' placeholder='Full Name' required />
                         <span className='selected-field'></span>
                     </div>
                     <div className='input-field'>
-                        <input type='email' placeholder='Email address' required/>
+                        <input name="email" type='email' placeholder='Email address' required />
                         <span className='selected-field'></span>
                     </div>
                 </div>
                 <div className='input-box'>
                     <div className='input-field'>
-                        <input type='number' placeholder='Mobile number' required/>
+                        <input email="mobile" type='number' placeholder='Mobile number' required />
                         <span className='selected-field'></span>
                     </div>
                     <div className='input-field'>
-                        <input type='text' placeholder='Email Subject' required/>
+                        <input name='email-subject' type='text' placeholder='Email Subject' required />
                         <span className='selected-field'></span>
                     </div>
                 </div>
                 <div className='textarea-field'>
-                    <textarea name="" id='' cols={30} rows={10} placeholder='Type Your Message Here' required></textarea>
+                    <textarea name="message" id='' cols={30} rows={10} placeholder='Type Your Message Here' required></textarea>
                     <span className='selected-field'></span>
                 </div>
                 <div className="w-full h-12 flex justify-center">
-                    <a href="#" type="submit" className="inline-flex justify-center items-center w-36 h-full text-lg rounded-lg border-solid font-bold tracking-widest relative overflow-hidden z-1 border-2 border-cyan bg-cyan text-dark-theme cursor-pointer hire-me-btn">Submit</a>
+                    <button href="#" type="submit" className="inline-flex justify-center items-center w-36 h-full text-lg rounded-lg border-solid font-bold tracking-widest relative overflow-hidden z-1 border-2 border-cyan bg-cyan text-dark-theme cursor-pointer hire-me-btn">Submit</button>
                 </div>
             </form>
         </section>

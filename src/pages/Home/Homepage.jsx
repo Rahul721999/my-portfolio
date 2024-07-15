307449a2bd838ca03bc2729acd521f566f8dc2a8
Rahul721999 <rrmondal12@gmail.com>
2024-07-15 11:27:13 +0530

    added: let's-talk button working now

--- src/pages/Home/Homepage.jsx
+++ src/pages/Home/Homepage.jsx
@@ -1,33 +1,34 @@
-import { useRef, useEffect } from "react";
+import { useRef } from "react";
 import { MediaIcons, MobilePhoto, Photo, useTypedEffect } from "../../components";
 import './Homepage.css';
+import { handleScrollToSection } from "../../script";
 
 const Home = () => {
     const el = useRef(null);
     useTypedEffect(el);
     return (
         <section id="home" className="flex flex-row justify-start items-center section-container">
-                <div className="laptop:w-3/5">
-                    <div className="flex justify-center items-center"><MobilePhoto/></div>
-                    <h1 className="text-center tablet:text-left tablet:text-6xl text-4xl font-bold leading-tight">
-                        <span className="intro-tag">Hi, I'm&nbsp;</span>
-                        <span className="name-tag"> Rahul Mondal</span>
-                    </h1>
-                    <h3 className="tablet:text-3xl text-2xl font-bold my-1 designation-tag">And I'm a - <span ref={el} className="gradient-text"></span></h3>
-                    <p className="text-pretty  flex italic tablet:text-lg text-sm laptop:text-left my-5 mb-10 ">
-                        Aspiring and versatile software artisan with a flair for AI and 3 year of experience in Rust programming.
-                        Eagerly pursuing a riveting software development role where creativity meats innovation,
-                        to craft cutting-edge solutions and contribute to transformative projects
-                    </p>
-                    <div className="w-80 h-12 flex justify-between">
-                        <a href="#" className="btn-type1">Hire Me</a>
-                        <a href="#" className="btn-type2">Let's Talk</a>
-                    </div>
-                    <div className="btn-container">
-                        <MediaIcons />
-                    </div>
+            <div className="laptop:w-3/5">
+                <div className="flex justify-center items-center"><MobilePhoto /></div>
+                <h1 className="text-center tablet:text-left tablet:text-6xl text-4xl font-bold leading-tight">
+                    <span className="intro-tag">Hi, I'm&nbsp;</span>
+                    <span className="name-tag"> Rahul Mondal</span>
+                </h1>
+                <h3 className="tablet:text-3xl text-2xl font-bold my-1 designation-tag">And I'm a - <span ref={el} className="gradient-text"></span></h3>
+                <p className="text-pretty  flex italic tablet:text-lg text-sm laptop:text-left my-5 mb-10 ">
+                    Aspiring and versatile software artisan with a flair for AI and 3 year of experience in Rust programming.
+                    Eagerly pursuing a riveting software development role where creativity meats innovation,
+                    to craft cutting-edge solutions and contribute to transformative projects
+                </p>
+                <div className="w-80 h-12 flex justify-between">
+                    <a href="#" className="btn-type1">Hire Me</a>
+                    <a href="#" className="btn-type2" onClick={(e) => handleScrollToSection(e, 'contact')}>Let's Talk</a>
                 </div>
-                <div className="w-2/5 flex justify-center items-center"><Photo/></div>
+                <div className="btn-container">
+                    <MediaIcons />
+                </div>
+            </div>
+            <div className="w-2/5 flex justify-center items-center"><Photo /></div>
         </section>
     )
 }
--- src/script.js
+++ src/script.js
@@ -1,3 +1,10 @@
+
+export const handleScrollToSection = (e, sectionId) => {
+    e.preventDefault();
+    document.getElementById(sectionId).scrollIntoView({ behavior: "smooth" });
+};
+
+
 // window.onscroll = () =>{
 //     let header = document.querySelector('header');
 //     header.classList.toggle('sticky', window.scrollY > 100);

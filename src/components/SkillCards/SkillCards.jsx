dd12eb2cd84bcdc0cc61bf3fc5548862d64285f8
Rahul721999 <rrmondal12@gmail.com>
2024-07-18 09:49:42 +0530

    Added: skillcards to the skillpage

--- src/components/SkillCards/SkillCards.jsx
+++ src/components/SkillCards/SkillCards.jsx
@@ -1,19 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './SkillCards.css';
 
-export default function SkillCards() {
+export default function SkillCards({title, skillArr}) {
   return (
     <div className='flex flex-col w-[250px] h-3/4  skill-cards rounded-lg p-6'>
-      <div className='text-[var(--text-color)] flex items-center justify-center text-xl card-title mb-4'>Frontend</div>
-      <div className='languages flex-col'>
-        <span className='py-2 px-2 rounded-lg'>HTML</span>
-        <span className='py-2 px-2 rounded-lg'>CSS</span>
-        <span className='py-2 px-2 rounded-lg'>JS</span>
-        <span className='py-2 px-2 rounded-lg'>ReactJs</span>
-        <span className='py-2 px-2 rounded-lg'>NodeJs</span>
-        <span className='py-2 px-2 rounded-lg'>Tailwind</span>
-        <span className='py-2 px-2 rounded-lg'>BootStrap</span>
-        <span className='py-2 px-2 rounded-lg'>Yew.rs</span>
+      <div className='text-[var(--text-color)] flex items-center justify-center text-xl card-title mb-4'>{title}</div>
+      <div className='languages'>
+        {
+          skillArr.map((skill, index)=>(
+            <span id={index} className='py-2 px-2 rounded-lg'>{skill}</span>
+          ))
+        }
       </div>
     </div>
 
--- src/pages/Skills/Skillspage.jsx
+++ src/pages/Skills/Skillspage.jsx
@@ -1,17 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Scroller } from '../../components';
 import { SkillCards } from '../../components';
 import './SkillsPage.css';
 
 export default function Skills() {
+    const [skills, setSkills] = useState([]);
+    useEffect(() => {
+        fetch('./skills.json')
+            .then(response => {
+                if (!response.ok) {
+                    throw new console.error('Failed to fetch skills');
+                }
+                return response.json();
+            })
+            .then(data => {setSkills(data.skills)})
+            .catch(error => {
+                console.log('There was a problem: ', error);
+            });
+    }, []);
     return (
         <section id='skills' className='section-container secondary-color'>
             <h1 className='text-4xl font-bold flex justify-center laptop:text-6xl items-center'>
-                My&nbsp;<span className='text-cyan'>skills</span>
+                My&nbsp;<span className='text-[var(--main-color)]'>skills</span>
             </h1>
             <Scroller />
-            <div className=' w-[90%] h-[80%] pb-6 pt-6 text-sm flex flex-col relative items-center cards-container'>
-                <SkillCards />
+            <div className='bg-[var(--bg-color)] w-[90%] h-[80%] pb-6 pt-6 text-sm flex flex-row tablet:justify-center items-center cards-container'>
+                {skills.map((category, index) => (
+                    <SkillCards key={index} title={category.category} skillArr={category.items} />
+                ))}
             </div>
         </section>
     )

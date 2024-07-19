17588abb6c905a377189c68a19333441062c9598
Rahul721999 <rrmondal12@gmail.com>
2024-07-19 15:43:04 +0530

    fix: skill-card grid view overlapping problem

--- src/components/SkillCards/SkillCards.jsx
+++ src/components/SkillCards/SkillCards.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import './SkillCards.css';
-export default function SkillCards({title, skillArr}) {
+
+export default function SkillCards({ title, skillArr }) {
+  const len = skillArr.length;
+
   return (
     <div className='flex flex-col w-[200px] laptop:w-[550px] h-[450px] skill-cards rounded-lg p-6'>
       <div className='text-[var(--text-color)] flex items-center justify-center text-xl card-title mb-4'>{title}</div>
-      <div className='languages'>
+      <div className={`languages ${len > 9 ? 'laptop:grid-view' : ''}`}>
         {
-          skillArr.map((skill, index)=>(
-            <span id={index} className='py-2 px-2 rounded-lg'>{skill}</span>
+          skillArr.map((skill, index) => (
+            <span key={index} className='py-2 px-2 rounded-lg'>{skill}</span>
           ))
         }
       </div>
     </div>
-
-  )
+  );
 }

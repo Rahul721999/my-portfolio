d7e613bee5cda29484f6bd721a030a7c750e4fa6
Rahul721999 <rrmondal12@gmail.com>
2024-07-25 13:26:28 +0530

    added: circle animation to profile photo

--- src/components/Photo/Photo.jsx
+++ src/components/Photo/Photo.jsx
@@ -1,30 +1,71 @@
 import React from 'react';
-import './Photo.css'
+import { motion } from 'framer-motion';
+import './Photo.css';
 
 const Photo = () => {
     return (
-        <div className="justify-center items-center rounded-full profile-photo w-40 tablet:w-fit">
-            <img
-                src='./profile_photo.png'
-                alt='desktop-profile'
-                className='rounded-full'
-            />
+        <div className="flex relative justify-center items-center w-40 h-40 tablet:w-fit tablet:h-fit ">
+            <motion.div
+                className="relative rounded-full w-full h-full flex justify-center items-center "
+                initial={{ scale: 0 }}
+                animate={{ opacity: 1, scale: 1 }}
+                transition={{
+                    delay: 2,
+                    duration: 0.5,
+                    ease: "easeIn"
+                }}
+            >
+                <img
+                    src='./profile_photo.png'
+                    alt='desktop-profile'
+                    className='rounded-full w-[90%] h-[90%] tablet:p-2 laptop:p-3'
+                />
+                <motion.svg
+                    viewBox="0 0 160 160"
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="absolute w-full h-full"
+                    fill="transparent"
+                >
+                    <motion.circle
+                        cx="80"
+                        cy="80"
+                        r="78"
+                        stroke="var(--text-color)"
+                        strokeWidth="1"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        initial={{ strokeDasharray: "2 120 25 20 10 2" }}
+                        animate={{
+                            strokeDasharray: [
+                                "2 120 25 20 10 2",
+                                "15 5 95 60 15 30",
+                                "15 10 35 50 2 60"
+                            ],
+                            rotate: [0, 360],
+                        }}
+                        transition={{
+                            duration: 20,
+                            repeat: Infinity,
+                            repeatType: "reverse",
+                        }}
+                    />
+                </motion.svg>
+            </motion.div>
         </div>
-    )
-}
+    );
+};
 
 const AboutImg = () => {
-
     return (
         <div className="justify-center items-center about-img-container relative">
             <img
                 src='./aboutMeImg.jpg'
                 alt='about-Img'
-                className='about-img justify-center items-center'
+                className='about-img justify-center items-center '
             />
             <span className='circle-spin'></span>
         </div>
-    )
-}
+    );
+};
 
 export { Photo, AboutImg };

a1580b6ed6f19636a04fc1c758c27132417a1256
Rahul721999 <rrmondal12@gmail.com>
2024-08-15 13:50:46 +0530

    added: service-cards in service-page

--- src/components/ServiceCard/ServiceCard.jsx
+++ src/components/ServiceCard/ServiceCard.jsx
@@ -1,7 +1,33 @@
-import React from 'react'
+import React from 'react';
+import './ServiceCard.css';
 
-export default function ServiceCard() {
+export default function ServiceCard({ title, description }) {
+  const service_category = title || "Unknown";
+  const service_description = description || "Unknown";
   return (
-    <div>ServiceCard</div>
+    // <div class="container h-2/3 w-1/5">
+    //   <div class="card">
+    //     <div class="front">
+    //       <p class="front-heading text-lg font-bold font-sans">{service_category}</p>
+    //     </div>
+    //     <div class="back">
+    //       <p class="back-heading text-lg font-bold font-sans">{service_category}</p>
+    //       <p className='text-center text-xs pl-8 pr-8'>{service_description}</p>
+    //     </div>
+    //   </div>
+    // </div>
+
+    /* ---------------------new card design---------------------*/
+    <section class="container w-1/5">
+      <div class="card items-center">
+        <div class="content w-full">
+          <p class="logo">-</p>
+          <div class="h6 font-semibold w-7">{service_category}</div>
+          <div class="hover_content">
+            <p className='text-sm tablet:text-base'>{service_description}</p>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
--- src/pages/Service/Servicepage.jsx
+++ src/pages/Service/Servicepage.jsx
@@ -1,10 +1,45 @@
-import React from 'react';
+import { React, useState, useEffect } from 'react';
+import { ServiceCard } from '../../components';
+import './ServicePage.css';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
 
-const Service = ()=> {
+const Service = () => {
+  const [services, setService] = useState([]);
+  useEffect(() => {
+    fetch('./services.json')
+      .then(response => {
+        if (!response.ok) {
+          throw new console.error('Failed to fetch skills');
+        }
+        return response.json();
+      })
+      .then(data => { setService(data.services) })
+      .catch(error => {
+        console.log('There was a problem: ', error);
+      });
+  }, []);
+
   return (
-    <section id='service' className='section-container'>
-        <h1 className='text-4xl font-bold flex justify-center laptop:text-6xl items-center'>My&nbsp;<span className='text-cyan'>Services</span></h1>
+    <section id="service" className="h-screen section-container">
+      <h1 className="text-4xl font-bold flex justify-center laptop:text-6xl items-center">
+        My&nbsp;<span className="text-cyan">Services</span>
+      </h1>
+      <div className=" mt-8 mb-8
+        w-full h-[70%] items-center justify-center
+        gap-10 tablet:gap-20 
+        service-cards-container">
+        {services.map((service, index) => (
+          <ServiceCard key={index} title={service.category} description={service.description} />
+        ))}
+      </div>
+      <div className="w-full h-12 flex justify-center">
+        <a className="btn-type1 text-sm">Projects!</a>
+      </div>
     </section>
+
   )
 }
 

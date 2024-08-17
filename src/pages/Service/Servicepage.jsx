364b073c5121d156ff502a3c3ffd2e39ae4c9962
Rahul721999 <rrmondal12@gmail.com>
2024-08-17 22:59:10 +0530

    WIP: adding projectCard to service page

--- src/pages/Service/Servicepage.jsx
+++ src/pages/Service/Servicepage.jsx
@@ -1,5 +1,5 @@
 import { React, useState, useEffect } from 'react';
-import { ServiceCard } from '../../components';
+import { ProjectCard, ServiceCard } from '../../components';
 import './ServicePage.css';
 import 'swiper/css';
 import 'swiper/css/navigation';
@@ -57,15 +57,35 @@ export default Service;
 
 
 function ViewProject({ show }) {
+  const [projects, setProjects] = useState([]);
+  useEffect(() => {
+    fetch('./projects.json')
+      .then(response => {
+        if (!response.ok) {
+          throw new console.error('Failed to fetch skills');
+        }
+        return response.json();
+      })
+      .then(data => { setProjects(data.projects) })
+      .catch(error => {
+        console.log('There was a problem loading projects: ', error);
+      });
+  }, []);
+
   return (
     <section id="service" className="h-screen section-container">
       <h1 className="text-4xl font-bold flex justify-center laptop:text-6xl items-center">
         My&nbsp;<span className="text-cyan">Projects</span>
       </h1>
-      <div className=" mt-8 mb-8
-        w-full h-[70%] items-center justify-center
-        gap-10 tablet:gap-20 
-        service-cards-container">
+      <div className="
+        project-cards-container
+        mt-8 mb-8
+        w-[78%] tablet:w-[90%] laptop:w-[70%] h-[65%] 
+        gap-10 tablet:gap-15
+        ">
+          {projects.map((project) =>(
+            <ProjectCard project={project}/>
+          ))}
       </div>
       <div className="w-full h-12 flex justify-center">
         <a className="btn-type1 text-sm" onClick={show}>Close</a>

7be1ffdd64488f6c9d73ee85f514dfb1c6bf7933
Rahul721999 <rrmondal12@gmail.com>
2024-08-19 10:43:31 +0530

    changes in skill cards

--- src/components/SkillCards/SkillCards.jsx
+++ src/components/SkillCards/SkillCards.jsx
@@ -7,13 +7,36 @@ export default function SkillCards({ title, skillArr }) {
   return (
     <div className='flex flex-col w-[200px] laptop:w-[550px] h-[450px] skill-cards rounded-lg p-6'>
       <div className='text-[var(--text-color)] flex items-center justify-center text-xl card-title mb-4'>{title}</div>
-      <div className={`languages ${len > 9 ? 'laptop:grid-view' : ''}`}>
+      <div className={`
+        
+        flex flex-col 
+        gap-1 
+        overflow-scroll 
+        ${len > 9 ? 'laptop:grid grid-cols-3' : ''}`}>
         {
           skillArr.map((skill, index) => (
-            <span key={index} className='py-2 px-2 rounded-lg'>{skill}</span>
+            <Tag key={index} className='py-2 px-2' tag_name={skill}/>
           ))
         }
       </div>
     </div>
   );
+}
+
+
+const Tag = ({ tag_name }) => {
+  return (
+      <p className='
+          w-32
+          bg-gradient-to-r from-[var(--main-color)]
+          rounded-md
+          whitespace-nowrap
+          items-center
+          px-3 py-1 mx-2
+          text-xs font-semibold 
+          min-w-20 max-h-6
+          text-[var(--bg-color)]'
+      > • {tag_name}
+      </p>
+  )
 }
